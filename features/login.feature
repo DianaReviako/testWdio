cfe45a0ec7bfc0d9a4cd1869440b842b7bd62308
Diana Reviako <diana.reviako@wolterskluwer.com>
2025-04-05 22:44:43 +0400

    Add tests

---
+++ features/login.feature
@@ -0,0 +1,12 @@
+Feature: The Internet Guinea Pig Website
+
+    Given I am on the login page
+
+  Scenario: As a user, I can log into the secure area
+    When I login with <username> and <password>
+    Then I should see a flash message saying <message>
+
+    Examples:
+      | username | password             | message                        |
+      | tomsmith | SuperSecretPassword! | You logged into a secure area! |
+      | foobar   | barfoo               | Your username is invalid!      |
---
+++ features/step-definitions/steps.js
@@ -0,0 +1,27 @@
+const { Given, When, Then } = require('@wdio/cucumber-framework');
+const { expect, $ } = require('@wdio/globals')
+
+const LoginPage = require('../pageobjects/login.page');
+const SecurePage = require('../pageobjects/secure.page');
+const { default: AllureReporter } = require('@wdio/allure-reporter');
+
+const pages = {
+    login: LoginPage
+}
+
+Given(/^I am on the (\w+) page$/, async (page) => {
+    AllureReporter.addStep(`I am on the ${page} page`);
+    await pages[page].open()
+});
+
+When(/^I login with (\w+) and (.+)$/, async (username, password) => {
+    AllureReporter.addStep(`I login with ${username} and ${password}`);
+    await LoginPage.login(username, password)
+});
+
+Then(/^I should see a flash message saying (.*)$/, async (message) => {
+    AllureReporter.addStep(`I should see a flash message saying ${message}`);
+    await expect(SecurePage.flashAlert).toBeExisting();
+    await expect(SecurePage.flashAlert).toHaveText(expect.stringContaining(message));
+});
+
